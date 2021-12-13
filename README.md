a04f34bd2278a864ebcbe231c02999bed302b5c8
lordbecvold <lordbecvold@gmail.com>
2021-12-13 13:53:25 +0100

    Portd to 1.18.1

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# becvold_core
-## This si my old spigot survival server core plugin (Ported form 1.14 to 1.16.4)
+# Minecraft spigot survival core plugin
 
-### Becvold core plugin features
-#### Working on mincraft 1.16.4
-#### Features
-#### 1)SetHome /sethome /home
-#### 2)OnePlayer sleep
-#### 3)Tab Death counter
-#### 4)Timer util (Can set day time etc)
-#### 5)Coordinates in hud
+# - Features
+* SetHome /sethome /home
+* OnePlayer sleep
+* Tab Death counter
+* Coordinates in hud
+* Ram usage monitor
+
+## Dependencies
+* Spigot
+   * Version: **1.18.1**
+   * [Website](https://getbukkit.org/)
---
+++ src/main/java/xyz/becvar/becvoldcore/coordinateshud/CoordinatesTimer.java
@@ -0,0 +1,35 @@
+package xyz.becvar.becvoldcore.coordinateshud;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.plugin.*;
+import org.bukkit.entity.*;
+import net.md_5.bungee.api.*;
+import net.md_5.bungee.api.chat.*;
+import org.bukkit.*;
+
+public class CoordinatesTimer {
+
+    public static void run(final Plugin plugin) {
+        Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, (Runnable)new Runnable() {
+            @Override
+            public void run() {
+                for (final Player p : plugin.getServer().getOnlinePlayers()) {
+                    if ((Utils.checkPlayerList(p) && !Utils.getDefaultOn()) || (!Utils.checkPlayerList(p) && Utils.getDefaultOn())) {
+                        final Location l = p.getLocation();
+                        final long gameTime = p.getWorld().getTime();
+                        long hours = gameTime / 1000L + 6L;
+                        hours %= 24L;
+                        if (hours == 24L) {
+                            hours = 0L;
+                        }
+                        final long minutes = gameTime % 1000L * 60L / 1000L;
+                        String mm = "0" + minutes;
+                        mm = mm.substring(mm.length() - 2, mm.length());
+                        p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new ComponentBuilder(ChatColor.GOLD + "XYZ: " + ChatColor.WHITE + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ() + " " + ChatColor.GOLD + String.format("%-10s", Utils.rpGetPlayerDirection(p)) + hours + ":" + mm).create());
+                    }
+                }
+            }
+        }, 0L, (long)Utils.getTicks());
+    }
+}
+
